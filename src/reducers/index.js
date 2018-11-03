5034aad92c51c99f8d4ea5af766c1786ef78488f
Kamil Tomczak <kamiltomcak@gmail.com>
2018-11-03 15:57:48 +0100

    Reducer fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
-import { FETCH_TODOS } from "../actions/types";
+import { combineReducers } from "redux";
 
-export default (state = {}, action) => {
-  switch (action.type) {
-    case FETCH_TODOS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
+import data from "./dataReducer";
+
+export default combineReducers({
+  data
+});
