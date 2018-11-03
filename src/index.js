6be54b6e6a28724db31cd7ff2230ab262fe36c5c
Kamil Tomczak <kamiltomcak@gmail.com>
2018-11-03 14:28:18 +0100

    Lighweight boilerplate

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-/**
- * React Starter Kit for Firebase
- * https://github.com/kriasoft/react-firebase-starter
- * Copyright (c) 2015-present Kriasoft | MIT License
- */
-
-/* @flow */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createHistory from 'history/createBrowserHistory';
-
-import App from './common/App';
-import createRelay from './createRelay';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <App history={createHistory()} createRelay={createRelay} />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
